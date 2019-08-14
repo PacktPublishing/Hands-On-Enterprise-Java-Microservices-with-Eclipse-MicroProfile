0eb1e7fa5f585d182689ef79718e5b847e85333c
Heiko W. Rupp <hwr@pilhuhn.de>
2019-08-14 11:55:41 +0200

    Expose more types of metrics more explicitly.

--- Chapter05-metrics/src/main/java/com/packt/microprofile_book/metrics/RestHandler.java
+++ Chapter05-metrics/src/main/java/com/packt/microprofile_book/metrics/RestHandler.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.Histogram;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Gauge;
@@ -20,15 +21,28 @@ import org.eclipse.microprofile.metrics.annotation.Timed;
 @ApplicationScoped
 public class RestHandler {
 
-  @Inject
-  @Metric(absolute = true)
-  private Counter justACounter;
+  @GET
+  @Path("/hello")
+  @Timed(name="helloTime", absolute = true, description = "Timing of the Hello call",
+         tags={"type=timer","app=shop"})
+  public String getHelloTimed() {
+    try {
+      Thread.sleep((long) (Math.random()*200.0));
+    } catch (InterruptedException e) {
+       // We don't care if the sleep is interrupted.
+    }
+    return "Hello World";
+  }
 
   @SuppressWarnings("unused")
   @Gauge(name = "aGauge", unit = MetricUnits.NONE)
   int myGauge() {
     return 42;
   }
+
+  @Inject
+  @Metric(absolute = true)
+  private Counter justACounter;
 
   @GET
   @Counted(name="aCounter", monotonic = true, absolute = true)
@@ -41,5 +55,33 @@ public class RestHandler {
     return "Yo!";
 
   }
+
+  @GET
+  @Path("/m")
+  @Metered(absolute = true)
+  public String getMetered() {
+    return "Metered called";
+  }
+
+
+  @GET
+  @Path("/c")
+  @Counted(monotonic = true, absolute = true)
+  public String getCounted() {
+    return "Counted called";
+  }
+
+  @Inject
+  @Metric(absolute = true)
+  private Histogram aHistogram;
+
+  @GET
+  @Path("/h")
+  public String getHistogram() {
+    aHistogram.update((int) (Math.random() * 10.0));
+    return "Histogram called";
+  }
+
+
 
 }
